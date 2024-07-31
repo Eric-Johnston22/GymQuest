059b43b38279d352f42129e8fcc63e379423f256
Eric-Johnston <eric.mjohnston@protonmail.com>
2024-07-31 15:28:09 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GymQuest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
